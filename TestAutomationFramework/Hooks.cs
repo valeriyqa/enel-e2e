5e97e61709a6fa26aae47a688ded4b771f39b6c8
Oleksii Khabarov <oleksii.khabarov@emotorwerks.com>
2018-03-21 14:54:53 +0200

    Add enviroment variables

--- TestAutomationFramework/Hooks.cs
+++ TestAutomationFramework/Hooks.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using System;
 using TechTalk.SpecFlow;
 
 namespace TestAutomationFramework
@@ -16,6 +17,49 @@ namespace TestAutomationFramework
         public Hooks(IObjectContainer objectContainer)
         {
             _objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void GetVariables()
+        {
+            string udpAndApiHost = "https://www.google.com.ua";
+            string webHost = "www.google.com";
+
+            try
+            {
+                udpAndApiHost = Environment.GetEnvironmentVariable("udpAndApiHost");
+                webHost = Environment.GetEnvironmentVariable("webHost");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Unable to get enviroment variables");
+            }
+
+            Console.WriteLine(udpAndApiHost);
+            Console.WriteLine(webHost);
+            try
+            {
+                Uri uri = new Uri(udpAndApiHost);
+                Uri uri2 = new Uri(webHost);
+
+                Console.WriteLine("Scheme = " + uri.Scheme);
+                Console.WriteLine("Delimiter = " + Uri.SchemeDelimiter);
+                Console.WriteLine("Host = " + uri.Host);
+                Console.WriteLine("Port = " + uri.Port);
+                Console.WriteLine("Path = " + uri.AbsolutePath);
+
+                Console.WriteLine("Scheme = " + uri2.Scheme);
+                Console.WriteLine("Delimiter = " + Uri.SchemeDelimiter);
+                Console.WriteLine("Host = " + uri2.Host);
+                Console.WriteLine("Port = " + uri2.Port);
+                Console.WriteLine("Path = " + uri2.AbsolutePath);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Incorrect URL format");
+            }
         }
 
         [BeforeScenario("web")]
