2c178407e3ccbfa3d56fc8d2993325ee6c442684
Oleksii Khabarov <oleksii.khabarov@emotorwerks.com>
2018-06-03 13:26:50 +0300

    Add several Locations tests

--- TestAutomationFramework/Features/B2B_UI/Locations.feature
+++ TestAutomationFramework/Features/B2B_UI/Locations.feature
@@ -4,6 +4,12 @@
 
 @b2b @web
 Scenario: B2B_Web_ Locations - Add location
+	Given I login to the system as "Oleksii" (b2b)
+	And I navigate to the "Locations" page (b2b)
+	When I click on the "Add Location" button (b2b)
+	And I populate the Location form with correct data (b2b)
+	And I click on the "Create" button (b2b)
+	Then Popup window with "Location created successfully" message and "done" status should be displayed (b2b)
 
 @b2b @web
 Scenario: B2B_Web_ Locations - Delete Location
@@ -15,4 +21,10 @@ Scenario: B2B_Web_ Locations - Create sublocations
 Scenario: B2B_Web_ Locations - Delete Sublocations
 
 @b2b @web
-Scenario: B2B_Web_ Add second location with existing Location name
+Scenario: B2B_Web_ Locations - Add second location with existing Location name
+	Given I login to the system as "Oleksii" (b2b)
+	And I navigate to the "Locations" page (b2b)
+	When I click on the "Add Location" button (b2b)
+	And I populate the Location form with correct data (b2b)
+	And I click on the "Create" button (b2b)
+	Then Popup window with "Location with name 'Test Location' already exists" message and "error" status should be displayed (b2b)
--- TestAutomationFramework/POM/B2bLocationPage.cs
+++ TestAutomationFramework/POM/B2bLocationPage.cs
@@ -1,10 +1,4 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestAutomationFramework.POM
 {
@@ -33,41 +27,5 @@ namespace TestAutomationFramework.POM
             var generalPage = new B2bGeneralPage(driver);
             generalPage.SelectFromListByValue(AssignRateSelect, value);
         }
-
-        //public void SelectTimeZoneByValue(string value)
-        //{
-        //    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        //    wait.Until(wd => TimeZoneSelect.GetAttribute("aria-owns").Length > 0);
-        //    driver.FindElement(By.ClassName("mat-select-trigger")).Click();
-        //    wait.Until(wd => driver.FindElement(By.ClassName("cdk-overlay-container")).FindElement(By.ClassName("ng-trigger")));
-
-        //    IList<IWebElement> AllDropDownList = driver.FindElements(By.ClassName("mat-option-text"));
-        //    foreach (var element in AllDropDownList)
-        //    {
-        //        if (element.Text.Contains(value))
-        //        {
-        //            element.Click();
-        //            break;
-        //        }
-        //    }
-        //}
-
-        //public void SelectAssignRateByValue(string value)
-        //{
-        //    var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        //    wait.Until(wd => AssignRateSelect.GetAttribute("aria-owns").Length > 0);
-        //    driver.FindElement(By.ClassName("mat-select-trigger")).Click();
-        //    wait.Until(wd => driver.FindElement(By.ClassName("cdk-overlay-container")).FindElement(By.ClassName("ng-trigger")));
-
-        //    IList<IWebElement> AllDropDownList = driver.FindElements(By.ClassName("mat-option-text"));
-        //    foreach (var element in AllDropDownList)
-        //    {
-        //        if (element.Text.Contains(value))
-        //        {
-        //            element.Click();
-        //            break;
-        //        }
-        //    }
-        //}
     }
 }
