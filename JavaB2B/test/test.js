4de1eb75ed8516d18c46554a6e23516d24be7212
valeriyqa <parkhval@gmail.com>
2019-12-30 21:39:22 +0200

    remove user edit and delete

--- JavaB2B/test/test.js
+++ JavaB2B/test/test.js
@@ -161,7 +161,7 @@ test('create new user', async (done) => {
   await done();
 });
 
-test('change user information', async (done) => {
+/*test('change user information', async (done) => {
   driver.takeScreenshot().then(function(data){
     const base64Data = data.replace(/^data:image\/png;base64,/,"")
     fs.writeFile("out.png", base64Data, 'base64', function(err) {
@@ -188,7 +188,7 @@ test ('delete user', async (done) => {
   await util.findByXpathAndClick(driver, page.confirmDeleteUserButton);
   await util.findByXpathAndClick(driver, page.viewUsersButton);
   await done();
-});
+});*/
 
 test('connect with stripe', async (done) => {
   await driver.wait(sleep(2000), 3000);
